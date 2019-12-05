1274fbf53674aee75336de35876f7a3229fa518a
Marco <marco_carrillo@yahoo.com>
2019-12-04 23:04:54 -0500

    Added click handlers/recognized button clicked/ created function to handled click event and edit the activity on the calendar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,17 @@
+//*******************************************************************/
+// When called, this functions sets up all functionality needed to  */
+// enter and edit the activity for the hour.                        */
+//*******************************************************************/
+
+function edit_activity(number){
+
+    // depending on which button was clicked, the time slot will be edited
+
+    button_clicked=event.currentTarget.id[event.currentTarget.id.length-1];
+
+    console.log(button_clicked)
+
+}
 
 //********************************************/
 // When called, returns military time hour   */
@@ -31,16 +45,13 @@ function get_military_hour() {
 function handle_minute() {
 // updating time and date --Just in case date is changed, it refreshes both date and time, not just time
 
+console.log("I am at the minute handler")
 document.getElementById("DisplayDate").textContent=moment().format('MMMM Do YYYY');
 document.getElementById("TimeShown").textContent="Time -- "+moment().format('h:mm a');
 
 //  get current time in military time (0 to 23 hours)
 
-var current_hour=get_military_hour();
-
-var secs=parseInt(moment().format('ss'));
-console.log(secs);
-
+var current_hour=get_military_hour()-12;
 
 //**********************************************************************/
 //  activity on the table is stored in a matrix of strings.
@@ -53,11 +64,19 @@ console.log(secs);
 for(var i=0;i<9;i++){
 
     if(current_hour-i-9>0) {   // That means the current slot (index+8) is in the past, disable it
-        document.getElementById(i.toString()).setAttribute("class","row border hr-past")
+        $("#"+i.toString()).attr("class","row border hr-past")  // attributes of the entire row
+        $("#button"+i.toString()).off("click")                  // eliminating any click handlers from button
+        $("#button"+i.toString()).prop("disabled",true)         // eliminating any click handlers from button
     } else if(current_hour-i-9===0){  // This means the current slot is the current hour
-        document.getElementById(i.toString()).setAttribute("class","row border hr-current")
+        $("#"+i.toString()).attr("class","row border hr-current")
+        $("#button"+i.toString()).off("click")                  // eliminating any click handlers from button
+        $("#button"+i.toString()).on("click",edit_activity)     // setting click handling variable
+        $("#button"+i.toString()).prop("disabled",false)        // eliminating any click handlers from button
     } else {  // This means the current slot (index+8) happens in the future.
-        document.getElementById(i.toString()).setAttribute("class","row border hr-future")
+        $("#"+i.toString()).attr("class","row border hr-future")
+        $("#button"+i.toString()).off("click")                  // eliminating any click handlers from button
+        $("#button"+i.toString()).on("click",edit_activity)     // setting click handling variable
+        $("#button"+i.toString()).prop("disabled",false)        // eliminating any click handlers from button
     }
 }
 
@@ -75,12 +94,8 @@ for(var i=0;i<9;i++){
 //**********************************************************************/
 
 function set_initial_interval(){
-
     var seconds=parseInt(moment().format('ss'));
-    console.log(seconds);
-
-    if(seconds===0){   // Exactly when the clock changes minute
-        console.log("cleaning stuff")
+    if(seconds===0){   // Exactly when the clock changes from one minute to the next
         clearInterval(IntervalHandler);  // Clears the 1 second interval
         handle_minute();                 // refreshes time
         Interval_Handler=setInterval(handle_minute,60000);   //  Sets the interval to each minute
@@ -93,8 +108,4 @@ function set_initial_interval(){
 //****************************************************************************************** */
 
 handle_minute();                            // Initial writing, gets the time and posts it
-
-var IntervalHandler=setInterval(set_initial_interval,1000);
-
-
-// write local time to the webpage
+var IntervalHandler=setInterval(set_initial_interval,1000);// write local time to the webpage
